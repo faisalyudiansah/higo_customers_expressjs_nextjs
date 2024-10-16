26124556c7f19755eff47d08abb102566812b4ae
Faisal Yudiansah <mfaisal.yudiansah@gmail.com>
2024-10-16 19:59:38 +0700

    feat: add table for data customer

--- fe/src/components/ui/tableCustomers.tsx
+++ fe/src/components/ui/tableCustomers.tsx
@@ -1,35 +1,18 @@
 "use client"
 
 import * as React from "react"
-import {
-    CaretSortIcon,
-    ChevronDownIcon,
-    DotsHorizontalIcon,
-} from "@radix-ui/react-icons"
+import { CaretSortIcon } from "@radix-ui/react-icons"
 import {
     ColumnDef,
-    ColumnFiltersState,
     SortingState,
     VisibilityState,
     flexRender,
     getCoreRowModel,
-    getFilteredRowModel,
     getPaginationRowModel,
     getSortedRowModel,
     useReactTable,
 } from "@tanstack/react-table"
-
 import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
-import {
-    DropdownMenu,
-    DropdownMenuCheckboxItem,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 import {
     Table,
@@ -39,231 +22,224 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import { useDispatch, useSelector } from "react-redux"
+import { AppDispatch, RootState } from "@/stores"
+import { fetchCustomerDataAPI } from "@/stores/slices/customerSlice"
+import { Customer } from "@/types/dataCustomers"
 
-const data: Payment[] = [
-    {
-        id: "m5gr84i9",
-        amount: 316,
-        status: "success",
-        email: "ken99@yahoo.com",
-    },
-    {
-        id: "3u1reuv4",
-        amount: 242,
-        status: "success",
-        email: "Abe45@gmail.com",
-    },
-    {
-        id: "derv1ws0",
-        amount: 837,
-        status: "processing",
-        email: "Monserrat44@gmail.com",
-    },
-    {
-        id: "5kma53ae",
-        amount: 874,
-        status: "success",
-        email: "Silas22@gmail.com",
-    },
-    {
-        id: "bhqecj4p",
-        amount: 721,
-        status: "failed",
-        email: "carmella@hotmail.com",
-    },
-]
+function debounce<T extends (...args: any[]) => void>(func: T, delay: number) {
+    let timeoutId: ReturnType<typeof setTimeout> | null = null;
 
-export type Payment = {
-    id: string
-    amount: number
-    status: "pending" | "processing" | "success" | "failed"
-    email: string
+    return function (...args: Parameters<T>) {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
+        timeoutId = setTimeout(() => {
+            func(...args);
+        }, delay);
+    };
 }
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<Customer>[] = [
     {
-        id: "select",
-        header: ({ table }) => (
-            <Checkbox
-                checked={
-                    table.getIsAllPageRowsSelected() ||
-                    (table.getIsSomePageRowsSelected() && "indeterminate")
-                }
-                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-                aria-label="Select all"
-            />
+        accessorKey: "customerID",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Customer ID
+                <CaretSortIcon className="h-4 w-4" />
+            </Button>
         ),
-        cell: ({ row }) => (
-            <Checkbox
-                checked={row.getIsSelected()}
-                onCheckedChange={(value) => row.toggleSelected(!!value)}
-                aria-label="Select row"
-            />
-        ),
-        enableSorting: false,
-        enableHiding: false,
-    },
-    {
-        accessorKey: "status",
-        header: "Status",
-        cell: ({ row }) => (
-            <div className="capitalize">{row.getValue("status")}</div>
-        ),
-    },
-    {
-        accessorKey: "email",
-        header: ({ column }) => {
-            return (
-                <Button
-                    variant="ghost"
-                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-                >
-                    Email
-                    <CaretSortIcon className="ml-2 h-4 w-4" />
-                </Button>
-            )
-        },
-        cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
-    },
-    {
-        accessorKey: "amount",
-        header: () => <div className="text-right">Amount</div>,
         cell: ({ row }) => {
-            const amount = parseFloat(row.getValue("amount"))
-
-            // Format the amount as a dollar amount
-            const formatted = new Intl.NumberFormat("en-US", {
+            const customerID: number = row.getValue("customerID")
+            return <div className="ml-10">{customerID}</div>
+        },
+    },
+    {
+        accessorKey: "gender",
+        header: "Gender",
+    },
+    {
+        accessorKey: "age",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Age
+                <CaretSortIcon className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => {
+            const age: number = row.getValue("age")
+            return <div className="ml-10">{age}</div>
+        },
+    },
+    {
+        accessorKey: "annual_income",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Annual Income
+                <CaretSortIcon className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => {
+            const annual_income: number = row.getValue("annual_income")
+            const formatted = new Intl.NumberFormat("id-ID", {
                 style: "currency",
-                currency: "USD",
-            }).format(amount)
-
-            return <div className="text-right font-medium">{formatted}</div>
+                currency: "IDR",
+            }).format(annual_income)
+            return <div className="ml-10">{formatted}</div>
         },
     },
     {
-        id: "actions",
-        enableHiding: false,
+        accessorKey: "spending_score",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Spending Score
+                <CaretSortIcon className="ml-2 h-4 w-4" />
+            </Button>
+        ),
         cell: ({ row }) => {
-            const payment = row.original
+            const spending_score: number = row.getValue("spending_score")
+            return <div className="ml-10">{spending_score}</div>
+        },
+    },
+    {
+        accessorKey: "profession",
+        header: "Profession",
 
-            return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <DotsHorizontalIcon className="h-4 w-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                            onClick={() => navigator.clipboard.writeText(payment.id)}
-                        >
-                            Copy payment ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View customer</DropdownMenuItem>
-                        <DropdownMenuItem>View payment details</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            )
+    },
+    {
+        accessorKey: "work_experience",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Work Experience
+                <CaretSortIcon className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => {
+            const work_experience: number = row.getValue("work_experience")
+            return <div className="ml-10">{work_experience}</div>
+        },
+    },
+    {
+        accessorKey: "family_size",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Family Size
+                <CaretSortIcon className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => {
+            const family_size: number = row.getValue("family_size")
+            return <div className="ml-10">{family_size}</div>
         },
     },
 ]
 
 export function TableCustomers() {
+    const dispatch = useDispatch<AppDispatch>()
+    const { customerData, customerLoading } = useSelector(
+        (state: RootState) => state.customerSlice
+    )
     const [sorting, setSorting] = React.useState<SortingState>([])
-    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    const [metaData, setMetaData] = React.useState({
+        page: 1,
+        limit: 5,
+        search: "",
+    })
+    const [searchTerm, setSearchTerm] = React.useState("")
+    const [totalPage, SetTotalPage] = React.useState(0)
+
+    const debouncedSearch = React.useMemo(
+        () =>
+            debounce((value: string) => {
+                setMetaData((prev) => ({ ...prev, search: value, page: 1 }))
+            }, 500),
         []
     )
-    const [columnVisibility, setColumnVisibility] =
-        React.useState<VisibilityState>({})
-    const [rowSelection, setRowSelection] = React.useState({})
+
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value
+        setSearchTerm(value)
+        debouncedSearch(value)
+    }
 
     const table = useReactTable({
-        data,
+        data: customerData?.data || [],
         columns,
         onSortingChange: setSorting,
-        onColumnFiltersChange: setColumnFilters,
         getCoreRowModel: getCoreRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
         getSortedRowModel: getSortedRowModel(),
-        getFilteredRowModel: getFilteredRowModel(),
-        onColumnVisibilityChange: setColumnVisibility,
-        onRowSelectionChange: setRowSelection,
         state: {
             sorting,
-            columnFilters,
-            columnVisibility,
-            rowSelection,
         },
     })
+
+    React.useEffect(() => {
+        const fetchCustomerData = async () => {
+            await dispatch(
+                fetchCustomerDataAPI(metaData.page, metaData.limit, metaData.search)
+            )
+        }
+        fetchCustomerData()
+    }, [metaData.page, metaData.search])
+
+    React.useEffect(() => {
+        if (customerData?.totalPages) {
+            SetTotalPage(customerData?.totalPages)
+        }
+    }, [customerData])
 
     return (
         <div className="w-full">
             <div className="flex items-center py-4">
                 <Input
-                    placeholder="Filter emails..."
-                    value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-                    onChange={(event) =>
-                        table.getColumn("email")?.setFilterValue(event.target.value)
-                    }
+                    placeholder="Search profession..."
                     className="max-w-sm"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
                 />
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="outline" className="ml-auto">
-                            Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        {table
-                            .getAllColumns()
-                            .filter((column) => column.getCanHide())
-                            .map((column) => {
-                                return (
-                                    <DropdownMenuCheckboxItem
-                                        key={column.id}
-                                        className="capitalize"
-                                        checked={column.getIsVisible()}
-                                        onCheckedChange={(value) =>
-                                            column.toggleVisibility(!!value)
-                                        }
-                                    >
-                                        {column.id}
-                                    </DropdownMenuCheckboxItem>
-                                )
-                            })}
-                    </DropdownMenuContent>
-                </DropdownMenu>
             </div>
             <div className="rounded-md border">
                 <Table>
                     <TableHeader>
                         {table.getHeaderGroups().map((headerGroup) => (
                             <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => {
-                                    return (
-                                        <TableHead key={header.id}>
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                    header.column.columnDef.header,
-                                                    header.getContext()
-                                                )}
-                                        </TableHead>
-                                    )
-                                })}
+                                {headerGroup.headers.map((header) => (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                    </TableHead>
+                                ))}
                             </TableRow>
                         ))}
                     </TableHeader>
                     <TableBody>
-                        {table.getRowModel().rows?.length ? (
+                        {table.getRowModel().rows.length ? (
                             table.getRowModel().rows.map((row) => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={row.getIsSelected() && "selected"}
-                                >
+                                <TableRow key={row.id}>
                                     {row.getVisibleCells().map((cell) => (
                                         <TableCell key={cell.id}>
                                             {flexRender(
@@ -280,7 +256,7 @@ export function TableCustomers() {
                                     colSpan={columns.length}
                                     className="h-24 text-center"
                                 >
-                                    No results.
+                                    {customerLoading ? " Loading..." : "No results."}
                                 </TableCell>
                             </TableRow>
                         )}
@@ -288,28 +264,29 @@ export function TableCustomers() {
                 </Table>
             </div>
             <div className="flex items-center justify-end space-x-2 py-4">
-                <div className="flex-1 text-sm text-muted-foreground">
-                    {table.getFilteredSelectedRowModel().rows.length} of{" "}
-                    {table.getFilteredRowModel().rows.length} row(s) selected.
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => setMetaData((prev) => ({ ...prev, page: prev.page - 1 }))}
+                    disabled={metaData.page === 1}
+                >
+                    Previous
+                </Button>
+                <div>
+                    {!customerLoading && (
+                        <>
+                            {metaData.page} of {totalPage}
+                        </>
+                    )}
                 </div>
-                <div className="space-x-2">
-                    <Button
-                        variant="outline"
-                        size="sm"
-                        onClick={() => table.previousPage()}
-                        disabled={!table.getCanPreviousPage()}
-                    >
-                        Previous
-                    </Button>
-                    <Button
-                        variant="outline"
-                        size="sm"
-                        onClick={() => table.nextPage()}
-                        disabled={!table.getCanNextPage()}
-                    >
-                        Next
-                    </Button>
-                </div>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => setMetaData((prev) => ({ ...prev, page: prev.page + 1 }))}
+                    disabled={metaData.page >= totalPage}
+                >
+                    Next
+                </Button>
             </div>
         </div>
     )
--- fe/src/stores/slices/customerSlice.ts
+++ fe/src/stores/slices/customerSlice.ts
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { Dispatch } from 'redux';
 import { GenderData } from '@/types/genderData';
 import { AgeData } from '@/types/ageData';
+import { CustomersResponse } from '@/types/dataCustomers';
 
 interface AuthState {
     genderData: GenderData[] | null
@@ -10,6 +11,9 @@ interface AuthState {
     ageData: AgeData[] | null
     ageError: string | null
     ageLoading: boolean
+    customerData: CustomersResponse | null
+    customerError: string | null
+    customerLoading: boolean
 }
 
 const initialState: AuthState = {
@@ -19,6 +23,9 @@ const initialState: AuthState = {
     ageData: null,
     ageError: null,
     ageLoading: true,
+    customerData: null,
+    customerError: null,
+    customerLoading: true,
 };
 
 export const customerSlice = createSlice({
@@ -31,6 +38,9 @@ export const customerSlice = createSlice({
         setAgeData: (state, action) => { state.ageData = action.payload; },
         setAgeError: (state, action) => { state.ageError = action.payload; },
         setAgeLoading: (state, action) => { state.ageLoading = action.payload; },
+        setCustomerData: (state, action) => { state.customerData = action.payload; },
+        setCustomerError: (state, action) => { state.customerError = action.payload; },
+        setCustomerLoading: (state, action) => { state.customerLoading = action.payload; },
     },
 });
 
@@ -41,7 +51,32 @@ export const {
     setAgeData,
     setAgeError,
     setAgeLoading,
+    setCustomerData,
+    setCustomerError,
+    setCustomerLoading,
 } = customerSlice.actions;
+
+export const fetchCustomerDataAPI = (page = 1, limit = 5, search : string) => async (dispatch: Dispatch): Promise<void> => {
+    try {
+        dispatch(setCustomerLoading(true))
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/customers?page=${page}&limit=${limit}&q=${search}`);
+        if (!response.ok) {
+            const errorResponse = await response.json();
+            throw new Error(errorResponse.message || 'Failed to fetch Customer data');
+        }
+        const data: CustomersResponse = await response.json();
+        dispatch(setCustomerData(data))
+    } catch (error) {
+        if (error instanceof Error) {
+            dispatch(setCustomerError(error.message || 'An unexpected error occurred'));
+        } else {
+            dispatch(setCustomerError('An unexpected error occurred'));
+        }
+        throw error
+    } finally {
+        dispatch(setCustomerLoading(false))
+    }
+};
 
 export const fetchGenderDataAPI = () => async (dispatch: Dispatch): Promise<void> => {
     try {
